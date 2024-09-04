5f2510344715fb1e5f6758f1cf7950c003103fb1
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-03 23:14:01 -0500

    Create ExtensionsDouble.cs

---
+++ Extensions/ExtensionsDouble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpUtils;
+
+public static class ExtensionsDouble
+{
+    /// <summary>
+    /// Checks to see if <paramref name="value"/> is an integer.
+    /// 
+    /// <para>
+    /// Since double.IsInteger(double d) is only in .NET 7, this extension 
+    /// will act as a helper for .NET 6 users.
+    /// </para>
+    /// </summary>
+    public static bool IsInteger(this double value) => (value % 1) == 0;
+}
