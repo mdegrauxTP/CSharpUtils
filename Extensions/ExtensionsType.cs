621738651f66d6967dd141ef0b58fc1fc4543a85
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-03 19:50:20 -0500

    Create ExtensionsType.cs

---
+++ Extensions/ExtensionsType.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpUtils;
+
+public static class ExtensionsType
+{
+    /// <summary>
+    /// Returns true if the <paramref name="type"/> is a whole number.
+    /// </summary>
+    public static bool IsWholeNumber(this Type type)
+    {
+        return
+            type == typeof(int) ||
+            type == typeof(long) ||
+            type == typeof(short) ||
+            type == typeof(byte) ||
+            type == typeof(sbyte) ||
+            type == typeof(uint) ||
+            type == typeof(ulong) ||
+            type == typeof(ushort);
+    }
+}
