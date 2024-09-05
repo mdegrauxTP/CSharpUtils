559e290ec36ab831c0b91cb80456bd5422ff3c77
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-09-04 21:38:14 -0500

    Update ExtensionsType.cs

--- Extensions/ExtensionsType.cs
+++ Extensions/ExtensionsType.cs
@@ -3,6 +3,30 @@
 public static class ExtensionsType
 {
     /// <summary>
+    /// Returns true if the <paramref name="type"/> is a numeric type
+    /// <para>For .NET 6.0 and later, consider using the INumber interface for a more generic approach.</para>
+    /// </summary>
+    public static bool IsNumericType(this Type @type)
+    {
+        HashSet<Type> numericTypes =
+        [
+            typeof(int),
+            typeof(float),
+            typeof(double),
+            typeof(long),
+            typeof(short),
+            typeof(ushort),
+            typeof(uint),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(byte),
+            typeof(sbyte)
+        ];
+
+        return numericTypes.Contains(type);
+    }
+
+    /// <summary>
     /// Returns true if the <paramref name="type"/> is a whole number.
     /// </summary>
     public static bool IsWholeNumber(this Type type)
