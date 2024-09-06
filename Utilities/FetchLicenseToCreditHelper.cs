c8835a7ca7e0f83056ad67a959dd59fdf175e519
mdegrauxTP <89838999+mdegrauxTP@users.noreply.github.com>
2024-09-06 13:36:30 +0200

    Create FetchLicenseToCreditHelper.cs
    Create static function that fetch licenses.txt file in the current directory that will be append to the credit.Txt

---
+++ Utilities/FetchLicenseToCreditHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Template.Template.CSharpUtils.Utilities;
+
+public class LicenseFetcher
+{
+    public static void FetchLicensesAndUpdateCredits()
+    {
+        string projectDirectory = Directory.GetCurrentDirectory();
+        string[] licenseFiles = Directory.GetFiles(projectDirectory, "license.txt", SearchOption.AllDirectories);
+        string creditsFilePath = Path.Combine(projectDirectory, "credits.txt");
+
+        using (StreamWriter creditsFile = new StreamWriter(creditsFilePath, append: true))
+        {
+            foreach (string licenseFile in licenseFiles)
+            {
+                string[] licenseContent = File.ReadAllLines(licenseFile);
+                foreach(var line in licenseContent)
+                {
+                    creditsFile.WriteLine(line);
+                }
+
+                creditsFile.WriteLine();
+            }
+        }
+
+        Console.WriteLine("All licenses have been appended to credits.txt.");
+    }
+}
